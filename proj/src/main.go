f36defab07818df41f6b6c7ee9d49b5796ebced5
JiaHaoFang <473993067@qq.com>
2021-06-01 15:46:11 +0800

    Change

--- proj/src/main.go
+++ proj/src/main.go
@@ -7,6 +7,6 @@ func main() {
 	// f, _ := os.Create("trace.out")
 	// defer f.Close()
 	// trace.Start(f)
-	algorithm.Compare(50, 50, 0.2, 10, 11, [2]int{1, 1}, [2]int{495, 495}, 0)
+	algorithm.Compare(50, 50, 0.2, 10, 11, [2]int{1, 1}, [2]int{49, 49}, 0)
 	// trace.Stop()
 }
